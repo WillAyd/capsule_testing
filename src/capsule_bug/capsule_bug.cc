d2120537cade004038b2644adb2b99510c85fd6c
Will Ayd <william.ayd@icloud.com>
2024-08-20 22:38:59 -0400

    Improve readability and error

--- src/capsule_bug/capsule_bug.cc
+++ src/capsule_bug/capsule_bug.cc
@@ -1,5 +1,5 @@
-#include <nanobind/nanobind.h>
 #include <nanoarrow/nanoarrow.hpp>
+#include <nanobind/nanobind.h>
 
 namespace nb = nanobind;
 
@@ -21,7 +21,7 @@ auto CheckData(nb::object obj) {
       ArrowArrayStreamGetSchema(stream.get(), schema.get(), &error));
 
   // Get the schema view for our child, which we assume is decimal
-  ArrowSchemaView schema_view;  
+  ArrowSchemaView schema_view;
   NANOARROW_THROW_NOT_OK(
       ArrowSchemaViewInit(&schema_view, schema->children[0], &error));
 
@@ -32,8 +32,7 @@ auto CheckData(nb::object obj) {
     nanoarrow::UniqueArrayView array_view;
     ArrowArrayViewInitFromSchema(array_view.get(), schema->children[0], &error);
     NANOARROW_THROW_NOT_OK(
-                           ArrowArrayViewSetArray(array_view.get(), chunk.children[0], &error));
-
+        ArrowArrayViewSetArray(array_view.get(), chunk.children[0], &error));
 
     struct ArrowBuffer buffer;
     ArrowBufferInit(&buffer);
@@ -41,23 +40,33 @@ auto CheckData(nb::object obj) {
     struct ArrowDecimal decimal;
     ArrowDecimalInit(&decimal, schema_view.decimal_bitwidth,
                      schema_view.decimal_precision, schema_view.decimal_scale);
-    
+
     ArrowArrayViewGetDecimalUnsafe(array_view.get(), 0, &decimal);
     NANOARROW_THROW_NOT_OK(ArrowDecimalAppendDigitsToBuffer(&decimal, &buffer));
-    if (strncmp(reinterpret_cast<const char*>(buffer.data), "12345678901234567890", buffer.size_bytes)) {
-      throw nb::value_error("first record did not match expectation");
+    const char first_value[] = "12345678901234567890";
+    if (strncmp(reinterpret_cast<const char *>(buffer.data), first_value,
+                buffer.size_bytes)) {
+      throw nb::value_error(
+          "first record did not match expectation - expected: " +
+          std::string(first_value) + " got: " +
+          std::string{reinterpret_cast<const char *>(buffer.data),
+                      buffer.size_bytes});
     }
     ArrowBufferReset(&buffer);
 
     ArrowArrayViewGetDecimalUnsafe(array_view.get(), 1, &decimal);
     NANOARROW_THROW_NOT_OK(ArrowDecimalAppendDigitsToBuffer(&decimal, &buffer));
-    if (strncmp(reinterpret_cast<const char*>(buffer.data), "-998765432109876543210", buffer.size_bytes)) {
-      throw nb::value_error("second record did not match expectation");
+    const char first_value[] = "-998765432109876543210";
+    if (strncmp(reinterpret_cast<const char *>(buffer.data), second_value,
+                buffer.size_bytes)) {
+      throw nb::value_error(
+          "second record did not match expectation - expected: " +
+          std::string(second_value) + " got: " +
+          std::string{reinterpret_cast<const char *>(buffer.data),
+                      buffer.size_bytes});
     }
     ArrowBufferReset(&buffer);
   }
 }
 
-NB_MODULE(capsule_bug, m) {
-  m.def("check", &CheckData);
-}
+NB_MODULE(capsule_bug, m) { m.def("check", &CheckData); }
