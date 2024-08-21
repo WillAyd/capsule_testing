b42480020fe45958bad0b6966198088dd09635cf
Will Ayd <william.ayd@icloud.com>
2024-08-20 21:47:52 -0400

    Initial commit

---
+++ src/capsule_bug/capsule_bug.cc
@@ -0,0 +1,63 @@
+#include <nanobind/nanobind.h>
+#include <nanoarrow/nanoarrow.hpp>
+
+namespace nb = nanobind;
+
+auto CheckData(nb::object obj) {
+  const auto maybe_capsule = nb::getattr(obj, "__arrow_c_stream__")();
+  const auto capsule = nb::cast<nb::capsule>(maybe_capsule);
+
+  auto c_stream = static_cast<ArrowArrayStream *>(
+      PyCapsule_GetPointer(capsule.ptr(), "arrow_array_stream"));
+  if (c_stream == nullptr) {
+    throw nb::value_error("Invalid PyCapsule provided");
+  }
+  nanoarrow::UniqueArrayStream stream{c_stream};
+
+  // Get schema from stream
+  nanoarrow::UniqueSchema schema;
+  ArrowError error;
+  NANOARROW_THROW_NOT_OK(
+      ArrowArrayStreamGetSchema(stream.get(), schema.get(), &error));
+
+  // Get the schema view for our child, which we assume is decimal
+  ArrowSchemaView schema_view;  
+  NANOARROW_THROW_NOT_OK(
+      ArrowSchemaViewInit(&schema_view, schema->children[0], &error));
+
+  ArrowErrorCode code;
+  nanoarrow::ViewArrayStream array_stream{stream.get(), &code, &error};
+
+  for (const auto &chunk : array_stream) {
+    nanoarrow::UniqueArrayView array_view;
+    ArrowArrayViewInitFromSchema(array_view.get(), schema->children[0], &error);
+    NANOARROW_THROW_NOT_OK(
+                           ArrowArrayViewSetArray(array_view.get(), chunk.children[0], &error));
+
+
+    struct ArrowBuffer buffer;
+    ArrowBufferInit(&buffer);
+
+    struct ArrowDecimal decimal;
+    ArrowDecimalInit(&decimal, schema_view.decimal_bitwidth,
+                     schema_view.decimal_precision, schema_view.decimal_scale);
+    
+    ArrowArrayViewGetDecimalUnsafe(array_view.get(), 0, &decimal);
+    NANOARROW_THROW_NOT_OK(ArrowDecimalAppendDigitsToBuffer(&decimal, &buffer));
+    if (strncmp(reinterpret_cast<const char*>(buffer.data), "12345678901234567890", buffer.size_bytes)) {
+      throw nb::value_error("first record did not match expectation");
+    }
+    ArrowBufferReset(&buffer);
+
+    ArrowArrayViewGetDecimalUnsafe(array_view.get(), 1, &decimal);
+    NANOARROW_THROW_NOT_OK(ArrowDecimalAppendDigitsToBuffer(&decimal, &buffer));
+    if (strncmp(reinterpret_cast<const char*>(buffer.data), "-998765432109876543210", buffer.size_bytes)) {
+      throw nb::value_error("second record did not match expectation");
+    }
+    ArrowBufferReset(&buffer);
+  }
+}
+
+NB_MODULE(capsule_bug, m) {
+  m.def("check", &CheckData);
+}
---
+++ tests/test_bug.py
@@ -0,0 +1,11 @@
+import pyarrow as pa
+
+import capsule_bug
+
+def test_bug():
+    schema = pa.schema([("col", pa.decimal128(38, 10))])
+    tbl = pa.Table.from_arrays([
+        pa.array(["1234567890.123456789", "-99876543210.987654321", None])
+    ], schema=schema)
+
+    capsule_bug.check(tbl)
